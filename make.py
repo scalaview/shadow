0a2f662712f8efbd54945cdd0246552f7dfc93cd
benko <benko@3cps.ca>
2016-10-27 11:36:29 +0800

    try

---
+++ make.py
@@ -0,0 +1,10 @@
+from plan import Plan
+
+cron = Plan("commands")
+
+cron.command('node /home/cps/dev/my/shadow/ghost/schemel.js', every='5.minute', output=
+             dict(stdout='/tmp/top_stdout.log',
+                  stderr='/tmp/top_stderr.log'))
+
+if __name__ == "__main__":
+    cron.run("write")
