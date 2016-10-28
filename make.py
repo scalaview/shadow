0df689bcb8735b6d861e4470a8b9ec534d57b513
scalaview <chailink100@gmail.com>
2016-10-28 08:41:08 +0800

    fix node not found

--- make.py
+++ make.py
@@ -6,12 +6,13 @@ if __name__ == "__main__":
   ROOT_PATH = os.path.dirname(os.path.abspath(__file__))
   cron = Plan("commands")
   # online
-  cron.command('node %s/ghost/online.js'%ROOT_PATH, every='1.day', at="hour.19 minute.30", output=
+  cron.command('/usr/local/bin/node %s/ghost/online.js'%ROOT_PATH, every='1.day', at="hour.21 minute.30", output=
                dict(stdout='%s/log/online_stdout.log'%ROOT_PATH,
                     stderr='%s/log/online_stderr.log'%ROOT_PATH))
   # offline
-  cron.command('node %s/ghost/offline.js'%ROOT_PATH, every='1.day', at="hour.8 minute.30", output=
+  cron.command('/usr/local/bin/node %s/ghost/offline.js'%ROOT_PATH, every='1.day', at="hour.7", output=
                dict(stdout='%s/log/offline_stdout.log'%ROOT_PATH,
                     stderr='%s/log/offline_stderr.log'%ROOT_PATH))
+
   cron.run("check")
-  # cron.run("write")
+  cron.run("write")
